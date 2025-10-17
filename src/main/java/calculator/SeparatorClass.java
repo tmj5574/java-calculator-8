fd4021cdbc6212be4db65a0448b8a23219e49a98
2021037156 <tmj5574@hanyang.ac.kr>
2025-10-17 20:17:53 +0900

    feat: create a class to store the separator

---
+++ src/main/java/calculator/SeparatorClass.java
@@ -0,0 +1,21 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeparatorClass {
+
+    private ArrayList<String> separator = new ArrayList<String>(List.of(",", ":"));
+
+    public void updateSeparator(String input){
+        if(!input.contains("\n")){
+            return;
+        }
+        separator.clear();
+        separator.add(input.substring(2,input.indexOf("\n")));
+    }
+
+    public ArrayList<String> getSeparator() {
+        return separator;
+    }
+}
