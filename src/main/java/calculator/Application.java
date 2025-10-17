0caa473bfd3d87f81a738dfd90e924b604448156
2021037156 <tmj5574@hanyang.ac.kr>
2025-10-17 20:37:25 +0900

    feat: call class function in main

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,18 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputClass inputClass = new InputClass();
+        SeparatorClass separatorClass = new SeparatorClass();
+        ExceptionClass exceptionClass = new ExceptionClass(separatorClass);
+        CalculateClass calculateClass = new CalculateClass(separatorClass);
+        //입력 받기
+        String input = inputClass.getInput();
+        //예외 체크하기
+        exceptionClass.checkInput(input);
+        //계산하기
+        int result = calculateClass.calculate(input);
+        System.out.print(result);
     }
 }
+
+
