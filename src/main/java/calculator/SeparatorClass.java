f8bb7f4e2c5fa3fd9922bdfc4f174b3d4c14fec0
2021037156 <tmj5574@hanyang.ac.kr>
2025-10-18 13:49:50 +0900

    fix: add escape character

--- src/main/java/calculator/SeparatorClass.java
+++ src/main/java/calculator/SeparatorClass.java
@@ -8,11 +8,11 @@ public class SeparatorClass {
     private ArrayList<String> separator = new ArrayList<String>(List.of(",", ":"));
 
     public void updateSeparator(String input){
-        if(!input.contains("\n")){
+        if(!input.contains("\\n")){
             return;
         }
         separator.clear();
-        separator.add(input.substring(2,input.indexOf("\n")));
+        separator.add(input.substring(2,input.indexOf("\\n")));
     }
 
     public ArrayList<String> getSeparator() {
